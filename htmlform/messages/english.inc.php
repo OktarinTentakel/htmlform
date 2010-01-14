6294259bcd21efb7db674800682df0fb8c033e20
Intarim <Intarim@cbfa61c1-1c26-4d95-b987-0948fc0fb3df>
2010-01-14 11:54:17 +0000

    Wiederherstellen aktuelle Version nach Fehlcommit

---
+++ htmlform/messages/english.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+define('MSG_CUSTOMCASE', '"%name%" has an invalid form.');
+define('MSG_REQUIRED', '"%name%" is required.');
+define('MSG_MINLENGTH', '"%name%" has a minimum size of %count% characters or options.');
+define('MSG_MAXLENGTH', '"%name%" has a maximum size of %count% characters or options.');
+define('MSG_RANGELENGTH', '"%name%" has a minimum size of %min% and a maximum size of %max% characters or options.');
+define('MSG_MIN', '"%name%" has a minimum size of %count%.');
+define('MSG_MAX', '"%name%" has a maximum size of %count%.');
+define('MSG_RANGE', '"%name%" has a minimum size of %min% and a maximum size of %max%.');
+define('MSG_EMAIL', '"%name%" has to be a valid eMail-address.');
+define('MSG_URL', '"%name%" has to be a valid URL.');
+define('MSG_DATE', '"%name%" has to be a valid date following the syntax &quot;mm/dd/yyyy&quot;.');
+define('MSG_DATE_ISO', '"%name%" has to be a valid date following the ISO-syntax &quot;yyyy-mm-dd&quot;.');
+define('MSG_DATE_DE', '"%name%" has to be a valid date following the German syntax &quot;dd.mm.yyyy&quot;.');
+define('MSG_NUMBER', '"%name%" has to be a number (can be decimal).');
+define('MSG_NUMBER_DE', '"%name%" has to be a number according to German notation (can be decimal).');
+define('MSG_DIGITS', '"%name%" must only contain digits.');
+define('MSG_CREDITCARD', '"%name%" has to be a valid creditcard number of 15 to 16 Digits, where the digit groups are seperated by &quot;-&quot;.');
+define('MSG_CHARACTERCLASS', '"%name%" contains forbidden characters (not in [%class%]).');
+
+?>

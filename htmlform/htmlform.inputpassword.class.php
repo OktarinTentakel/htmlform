d150194c0bd12c2307c7888ef824b56def284fd8
Intarim <Intarim@cbfa61c1-1c26-4d95-b987-0948fc0fb3df>
2010-01-14 11:55:01 +0000

    Wiederherstellen aktuelle Version nach Fehlcommit

---
+++ htmlform/htmlform.inputpassword.class.php
@@ -0,0 +1,58 @@
+<?php
+
+//---|includes----------
+
+require_once('htmlform.inputtext.class.php');
+require_once('htmlform.label.class.php');
+
+
+
+//---|class----------
+
+class InputPassword extends InputText{
+	// ***
+	protected function __construct($name, $id = ''){
+		parent::__construct($name, $id);
+	}
+	
+	
+	
+	public static function get($name, $id = ''){
+		$res = new InputPassword($name, $id);
+		return $res;
+	}
+	// ***
+	
+	
+	
+	//---|output----------
+	
+	public function doRender(){
+		$label = ($this->label != '') ? Label::get($this)->doRender() : '';
+	
+		return
+			 '<div class="'.$this->printWrapperClasses().'">'
+				.$label
+				.'<div class="'.parent::WIDGETCLASS.'">'
+					.'<input'
+						.$this->printId()
+						.$this->printName()
+						.' type="password"'
+						.' value="'.$this->text.'"'
+						.$this->printSize()
+						.$this->printMaxLength()
+						.$this->printCssClasses()
+						.$this->printJsEventHandler()
+						.$this->printTabindex()
+						.$this->printReadonly()
+						.$this->printDisabled()
+						.$this->masterForm->printSlash()
+					.'>'
+				.'</div>'
+				.$this->masterForm->printFloatBreak()
+			.'</div>'
+		;
+	}
+}
+
+?>

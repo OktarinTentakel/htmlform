972206bb1578be116022d7806a53f15f3902891a
Intarim <Intarim@cbfa61c1-1c26-4d95-b987-0948fc0fb3df>
2010-01-14 11:54:54 +0000

    Wiederherstellen aktuelle Version nach Fehlcommit

---
+++ htmlform/htmlform.inputfile.class.php
@@ -0,0 +1,80 @@
+<?php
+
+//---|includes----------
+
+require_once('htmlform.inputtext.class.php');
+require_once('htmlform.label.class.php');
+
+
+
+//---|class----------
+
+class InputFile extends InputText{
+	// ***
+	private $accept;
+	
+	protected function __construct($name, $id = ''){
+		parent::__construct($name, $id);
+		
+		$this->accept = '';
+	}
+	
+	
+	
+	public static function get($name, $id = ''){
+		$res = new InputFile($name, $id);
+		return $res;
+	}
+	// ***
+	
+	
+	
+	//--|setter----------
+	
+	public function setAccept($accept){
+		if( preg_match('/^(application|audio|image|multipart|text|video)\/(\*|[a-zA-Z\-]+)$/i', $accept) ){
+			$this->accept = $accept;
+		}
+		
+		return $this;
+	}
+	
+	
+	
+	//---|output----------
+	
+	private function printAccept(){
+		return ($this->accept == '') ? '' : ' accept="'.$this->accept.'"';
+	}
+	
+	
+	
+	public function doRender(){
+		$label = ($this->label != '') ? Label::get($this)->doRender() : '';
+	
+		return
+			 '<div class="'.$this->printWrapperClasses().'">'
+				.$label
+				.'<div class="'.parent::WIDGETCLASS.'">'
+					.'<input'
+						.$this->printId()
+						.$this->printName()
+						.' type="file"'
+						.$this->printAccept()
+						.$this->printSize()
+						.$this->printMaxLength()
+						.$this->printCssClasses()
+						.$this->printJsEventHandler()
+						.$this->printTabindex()
+						.$this->printReadonly()
+						.$this->printDisabled()
+						.$this->masterForm->printSlash()
+					.'>'
+				.'</div>'
+				.$this->masterForm->printFloatBreak()
+			.'</div>'
+		;
+	}
+}
+
+?>
